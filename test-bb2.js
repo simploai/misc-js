2f48171dfd726c65d360116a744eaa695874507b
simplo.ai <155739372+simploai@users.noreply.github.com>
2024-10-27 12:15:47 +0100

    Create test-bb2.js

---
+++ test-bb2.js
@@ -0,0 +1,11 @@
+    // Hitta och dölj element med klasser som börjar på 'portal svelte-'
+    document.querySelectorAll("[class^='portal svelte-']").forEach(element => {
+        console.log("Hittad portal-komponent:", element); // Logga elementet
+        element.style.display = 'none';
+    });
+
+    // Hitta och dölj element med klasser som börjar på 'free-footer svelte-'
+    document.querySelectorAll("[class^='free-footer svelte-']").forEach(element => {
+        console.log("Hittad free-footer-komponent:", element); // Logga elementet
+        element.style.display = 'none';
+    });
