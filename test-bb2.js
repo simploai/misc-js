92d4410200e7d56621dc2af3fb32234c83651bf0
simplo.ai <155739372+simploai@users.noreply.github.com>
2024-10-29 12:48:44 +0100

    Update test-bb2.js

--- test-bb2.js
+++ test-bb2.js
@@ -1,11 +1,9 @@
-    // Hitta och dölj element med klasser som börjar på 'portal svelte-'
-    document.querySelectorAll("[class^='portal svelte-']").forEach(element => {
-        console.log("Hittad portal-komponent:", element); // Logga elementet
-        element.style.display = 'none';
-    });
+document.querySelectorAll("[class^='portal svelte-']").forEach(element => {
+    console.log("Hittad portal-komponent:", element); // Logga elementet
+    element.style.display = 'none';
+});
 
-    // Hitta och dölj element med klasser som börjar på 'free-footer svelte-'
-    document.querySelectorAll("[class^='free-footer svelte-']").forEach(element => {
-        console.log("Hittad free-footer-komponent:", element); // Logga elementet
-        element.style.display = 'none';
-    });
+document.querySelectorAll("[class^='free-footer svelte-']").forEach(element => {
+    console.log("Hittad free-footer-komponent:", element); // Logga elementet
+    element.style.display = 'none';
+});
