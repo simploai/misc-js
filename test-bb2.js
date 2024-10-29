fa9b4e35cb7aae285c29818b70434f5182d4d71b
simplo.ai <155739372+simploai@users.noreply.github.com>
2024-10-29 17:46:47 +0100

    Delete test-bb2.js

--- test-bb2.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("[class^='portal svelte-']").forEach(element => {
-    console.log("Hittad portal-komponent:", element); // Logga elementet
-    element.style.display = 'none';
-});
-
-document.querySelectorAll("[class^='free-footer svelte-']").forEach(element => {
-    console.log("Hittad free-footer-komponent:", element); // Logga elementet
-    element.style.display = 'none';
-});
