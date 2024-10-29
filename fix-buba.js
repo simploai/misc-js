981ecdfaaa680503a0278eb530845aa8fcac4602
simplo.ai <155739372+simploai@users.noreply.github.com>
2024-10-29 12:50:09 +0100

    Create fix-buba.js

---
+++ fix-buba.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("[class^='portal svelte-']").forEach(element => {
+    element.style.display = 'none';
+});
+
+document.querySelectorAll("[class^='free-footer svelte-']").forEach(element => {
+    element.style.display = 'none';
+});
